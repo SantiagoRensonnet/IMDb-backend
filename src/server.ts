369068ce6fe233ba8b7d9f9f5d1a680fa91d2355
Santiago <santirensonnet@gmail.com>
2023-05-21 16:15:12 +0200

    allow cross origin config #3

--- src/server.ts
+++ src/server.ts
@@ -47,22 +47,7 @@ var allowedOrigins = [
   "http://localhost:5173",
   "https://imdb-fullstack-app.netlify.app",
 ];
-app.use(
-  cors({
-    origin: function (origin: any, callback: any) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 //*********************************************
 
 //****************************************************************************
